9e9bb6e41b5e44e006ac60116e00066436264adc
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2019-10-04 19:37:33 +0200

    Update php.js

--- php.js/php.js
+++ php.js/php.js
@@ -1,13 +1,5 @@
 var location = "php.js/php.js";
 
-function remove(str){
-  if(str === null || str == ''){
-    return;
-  } else {
-    return str.replace(/<[^>]*>/g, '');
-  }
-}
-
 function exit(message){
   document.getElementByTagsName('html').innerHTML = message.toString() + '\n<script src="' + location + '"></script>'
 }
