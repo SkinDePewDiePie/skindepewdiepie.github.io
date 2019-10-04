e462e57b30d7fcff5d97a220cffc97f65a7f61bb
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2019-10-04 19:23:24 +0200

    Create php.js

---
+++ php.js/php.js
@@ -0,0 +1,19 @@
+var location = "php.js/php.js";
+
+function remove(str){
+  if(str === null || str == ''){
+    return;
+  } else {
+    return str.replace(/<[^>]*>/g, '');
+  }
+}
+
+function exit(message){
+  remove(document.getElementByTagsName('html').innerHTML));
+  document.write(message.toString());
+  document.write('<script src="' + location + '"></script>');
+}
+
+function die(message){
+  exit(message);
+}
