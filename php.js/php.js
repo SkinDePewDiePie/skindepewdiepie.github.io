74ce41a539ec7867df6d53359996225f48f29501
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2019-10-04 19:40:16 +0200

    Update php.js

--- php.js/php.js
+++ php.js/php.js
@@ -1,9 +1,1 @@
-var location = "php.js/php.js";
 
-function exit(message){
-  document.getElementByTagsName('html').innerHTML = message.toString() + '\n<script src="' + location + '"></script>';
-}
-
-function die(message){
-  exit(message);
-}
