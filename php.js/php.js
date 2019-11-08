3c7a7be960b0f7054e3507992ac7c7ba9ffaccfe
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2019-11-08 08:32:00 +0100

    Update php.js

--- php.js/php.js
+++ php.js/php.js
@@ -1,4 +1,5 @@
 function echo(message){
   message = message.toString();
+  if(message.indexOf("\n") !== -1) message.replace("\n", "<br>"); alert("Saut de ligne détécté !");
   document.write(message);
 }
