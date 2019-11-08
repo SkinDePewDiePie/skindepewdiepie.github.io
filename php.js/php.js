1f45e41fcc250c91abb12c7ca6d3203f9cd2bd74
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2019-11-08 08:36:11 +0100

    Update php.js

--- php.js/php.js
+++ php.js/php.js
@@ -1,5 +1,14 @@
+throw new Alert("Do not use it! It's a beta for PHP.js, you can download in test in local ! Not online okay!");
+
 function echo(message){
   message = message.toString();
   if(message.indexOf("\n") !== -1) message.replace("\n", "<br>"); alert("Saut de ligne détécté !");
   document.write(message);
 }
+
+function replace(message, replaceStr, string){
+  message = message.toString();
+  replaceStr = replaceStr.toString();
+  string = string.toString();
+  
+  message.replace(replaceStr, string);
